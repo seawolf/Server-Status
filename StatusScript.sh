7d5cbd4ddf3d10185e66a32527c5782c5cd23e24
Ben Arnold <ben@seawolfsanctuary.com>
2011-10-13 21:32:08 +0100

    Add: AIDE check and email results

--- StatusScript.sh
+++ StatusScript.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+HOST=`cat /etc/hostname`
+
+DM=`date +%d\ %B`
+YYMMDD=`date +%Y-%M-%d`
+HHMM=`date +%H%M`
+
+aide --check > /var/log/aide/check-$YYMMDD\_$HHMM.txt
+mail -s "AIDE Report for $HOST - $DM" $WEBMASTER /var/log/aide/check-$YYMMDD\_$HHMM.txt
