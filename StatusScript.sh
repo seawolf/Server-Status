cf9953959775d7c70f7bf07195fcc2cef2385ed6
Ben Arnold <ben@seawolfsanctuary.com>
2011-10-13 22:51:09 +0100

    Fix: quoting

--- StatusScript.sh
+++ StatusScript.sh
@@ -7,4 +7,4 @@ YYMMDD=`date +%Y-%m-%d`
 HHMM=`date +%H%M`
 
 aide --check > /var/log/aide/check-$YYMMDD\_$HHMM.txt
-cat /var/log/aide/check-$YYMMDD\_$HHMM.txt | mail -s \"AIDE Report for $HOST - $DM\" $WEBMASTER
+cat /var/log/aide/check-$YYMMDD\_$HHMM.txt | mail -s "AIDE Report for $HOST - $DM " $WEBMASTER
