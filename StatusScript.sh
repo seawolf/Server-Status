60b7d9c4bd44ededbc8c74977179e811e0137ed8
Ben Arnold <ben@seawolfsanctuary.com>
2011-10-16 11:28:45 +0100

    Fix: AIDE output to file

--- StatusScript.sh
+++ StatusScript.sh
@@ -7,5 +7,5 @@ DM=`date +%d\ %B`
 YYMMDD=`date +%Y-%m-%d`
 HHMM=`date +%H%M`
 
-aide --check > /var/log/aide/check-$YYMMDD\_$HHMM.txt
+aide --check -r /var/log/aide/check-$YYMMDD\_$HHMM.txt
 cat /var/log/aide/check-$YYMMDD\_$HHMM.txt | mail -s "AIDE Report for $HOST - $DM " $WEBMASTER
