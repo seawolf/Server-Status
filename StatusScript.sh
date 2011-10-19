d248f4a5ce0dafbbc4f4f530735bdb9f9e5354ba
Ben Arnold <ben@seawolfsanctuary.com>
2011-10-19 07:06:25 +0100

    ...

--- StatusScript.sh
+++ StatusScript.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 HOST=`hostname`
-WEBMASTER=`cat /root/Webmaster.txt` # add email address in that file
+WEBMASTER=`cat /root/Status-Script/Webmaster.txt` # add email address in that file
 
 DM=`date +%d\ %B`
 YYMMDD=`date +%Y-%m-%d`
 HHMM=`date +%H%M`
 
-aide --check -report="file:///var/log/aide/check-$YYMMDD\_$HHMM.txt"
+aide --check --report="file:///var/log/aide/check-$YYMMDD\_$HHMM.txt"
 cat /var/log/aide/check-$YYMMDD\_$HHMM.txt | mail -s "AIDE Report for $HOST - $DM " $WEBMASTER
