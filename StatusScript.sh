40bfee95ce72b88a2e26cb80b82d0e703946f023
Ben Arnold <ben@seawolfsanctuary.com>
2011-10-25 22:13:18 +0100

    Rev: mailx -> mail Rev: passing report to `mail` Fix: email look-up path

--- StatusScript.sh
+++ StatusScript.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 HOST=`hostname`
-WEBMASTER=`cat /root/Status-Script/Webmaster.txt` # add email address in that file
+WEBMASTER=`cat /root/Server-Status/Webmaster.txt` # add email address in that file
 
 DM=`date +%d\ %B`
 YYMMDD=`date +%Y-%m-%d`
 HHMM=`date +%H%M`
 
 /usr/sbin/aide --check >> /var/log/aide/check-$YYMMDD\_$HHMM.txt
-/bin/mailx -s "AIDE Report for $HOST - $DM " $WEBMASTER < /var/log/aide/check-$YYMMDD\_$HHMM.txt
+cat /var/log/aide/check-$YYMMDD\_$HHMM.txt | /bin/mail -s "AIDE Report for $HOST - $DM " $WEBMASTER
